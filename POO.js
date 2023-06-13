64478a913d2c08c583bab653e47fe8eec599b8c9
Sofia Lazarte <sofi.lazarte.sl@gmail.com>
2023-06-13 17:26:08 -0300

    punto uno y dos

---
+++ POO.js
@@ -0,0 +1,59 @@
+// punto uno
+/*class Auto 
+{
+    constructor(color,marca, modelo)
+    {
+        this.color= color;
+        this.marca= marca;
+        this.modelo= modelo;
+    }
+        
+    apagado()
+    {
+        document.write("el auto esta apagado");
+    }
+
+    encendido()
+    {
+        document.write("el auto esta encendido");
+    }
+}
+
+let autoUno = new Auto('negro', 'ford', 2018);
+autoUno.apagado();
+autoUno.encendido();
+
+//punto dos
+
+class Cuenta 
+{
+    constructor(titular, saldo)
+    {
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    ingresar(dinero)
+    {
+        this.saldo = this.saldo + dinero; 
+    }
+
+    extraer(dinero)
+    {
+        this.saldo = this.saldo - dinero; 
+    }
+
+    informar()
+    {
+        console.log(`la cuenta titular de ${this.titular}, tiene un saldo actual de ${this.saldo}`);
+    }
+
+
+}
+
+let cuentaUno = new Cuenta('SOFIA', 500);
+cuentaUno.informar();
+cuentaUno.ingresar(50);
+cuentaUno.extraer(100);
+cuentaUno.informar();
+*/
